b97b2916f4a21ec9f68aa3ce7d288b06c2a2f33c
Thanuga Soysa <thanugasoysa2003@gmail.com>
2024-06-23 18:04:38 +0530

    Subject Screen Update

--- app/src/main/java/com/example/studysmart/MainActivity.kt
+++ app/src/main/java/com/example/studysmart/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.studysmart.ui.dashboard.DashboardScreen
+import com.example.studysmart.ui.subject.SubjectScreen
 
 
 class MainActivity : ComponentActivity() {
@@ -19,7 +20,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            DashboardScreen()
+            SubjectScreen()
         }
     }
 }
---
+++ app/src/main/java/com/example/studysmart/ui/subject/SubjectScreen.kt
@@ -0,0 +1,89 @@
+package com.example.studysmart.ui.subject
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.LargeTopAppBar
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.material3.IconButton
+
+@Composable
+fun SubjectScreen() {
+    Scaffold(
+        topBar = {
+            SubjectScreenTopBar(
+                title = "English",
+                onBackButtonClick = { },
+                onDeleteButtonClick = { },
+                onEditButtonClick = { }
+            )
+        }
+    ) { paddingValues ->
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+        ) {
+            // Content goes here
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun SubjectScreenTopBar(
+    title: String,
+    onBackButtonClick: () -> Unit,
+    onDeleteButtonClick: () -> Unit,
+    onEditButtonClick: () -> Unit
+) {
+    LargeTopAppBar(
+        navigationIcon = {
+            IconButton(onClick = onBackButtonClick) {
+                Icon(
+                    imageVector = Icons.Default.ArrowBack,
+                    contentDescription = "Navigate back"
+                )
+            }
+        },
+        title = {
+            Text(
+                text = title,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis,
+                style = MaterialTheme.typography.headlineSmall
+            )
+        },
+        actions = {
+            IconButton(onClick = onDeleteButtonClick) {
+                Icon(
+                    imageVector = Icons.Default.Delete,
+                    contentDescription = "Delete Subject"
+                )
+            }
+            IconButton(onClick = onEditButtonClick) {
+                Icon(
+                    imageVector = Icons.Default.Edit,
+                    contentDescription = "Edit Subject"
+                )
+            }
+        }
+    )
+}
+
+
+@Composable
+private fun SubjectOverviewScreen() {
+
+}
