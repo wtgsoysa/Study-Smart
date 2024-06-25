2b9e8d39bb82e8b3c4f2d544cee49bc9ad99872b
Thanuga Soysa <thanugasoysa2003@gmail.com>
2024-06-25 23:53:48 +0530

    Update Count Card to SubjectScreen

--- app/src/main/java/com/example/studysmart/ui/subject/SubjectScreen.kt
+++ app/src/main/java/com/example/studysmart/ui/subject/SubjectScreen.kt
@@ -1,12 +1,18 @@
 package com.example.studysmart.ui.subject
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.LargeTopAppBar
@@ -14,9 +20,15 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.compose.material3.IconButton
+import androidx.compose.runtime.remember
+import com.example.studysmart.ui.components.CountCard
 
 @Composable
 fun SubjectScreen() {
@@ -82,8 +94,52 @@ private fun SubjectScreenTopBar(
     )
 }
 
-
 @Composable
-private fun SubjectOverviewScreen() {
-
+private fun SubjectOverviewScreen(
+    modifier: Modifier,
+    studieHours: String,
+    goalHours: String,
+    progress: Float,
+    percentageProgress: Int
+) {
+    val precentageProgress = remember(progress) {
+        (progress * 100).toInt().coerceIn(0,100)
+    }
+    Row(
+        modifier =modifier,
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
+    ){
+        CountCard(
+            modifier = Modifier.weight(1f),
+            headingText = "Goal Study Hours",
+            count = goalHours
+        )
+        Spacer(modifier = Modifier.size(8.dp))
+        CountCard(
+            modifier = Modifier.weight(1f),
+            headingText = "Completed Study Hours",
+            count = studieHours
+        )
+        Box(
+            modifier = Modifier.weight(1f),
+            contentAlignment = Alignment.Center
+        ){
+            CircularProgressIndicator(
+                modifier = Modifier.fillMaxSize(),
+                progress = 1f,
+                strokeWidth = 4.dp,
+                color = MaterialTheme.colorScheme.primary
+            )
+            CircularProgressIndicator(
+                modifier = Modifier.fillMaxSize(),
+                progress = progress,
+                strokeWidth = 4.dp,
+                color = MaterialTheme.colorScheme.primary
+            )
+            Text(text = "$percentageProgress%")
+        }
+    }
 }
+
+
